240e9aeee00366a4d1315b246adf7f7e9ddacf66
Tudor <if20376@bristol.ac.uk>
2022-03-06 15:08:59 +0000

    Some styling done

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,26 +2,43 @@ import React,{useState} from 'react'
 import axios from 'axios'
 
 function App() {
-
-  const weatherURL=  'https://api.openweathermap.org/data/2.5/weather?q=London&appid=c84660ba35fa7381c7cd2d4251ed65e8'
+  const [data,setData] = useState({})
+  const [location, setLocation] = useState('') 
+  const weatherURL=  `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c84660ba35fa7381c7cd2d4251ed65e8`
   return (
     <div className='App'>
       <div className='container'>
         <div className='top'>
+
           <div className='location'>
             <p>London</p>
             </div>
+
             <div className='temp'>
               <h1>10°C</h1>
               </div>
+
               <div className='description'>
                 <p>Cloudy</p>
                 </div>
           </div>
         <div className='bottom'>
+
           <div className='feels like'>
-            <p>8°C</p>
+            <p className='bold'>8°C</p>
+            <p>Feels like</p>
             </div>
+
+            <div className='humidity'>
+              <p className='bold'>50%</p>
+              <p>Humidity</p>
+              </div>
+
+              <div className='wind'>
+                <p className='bold'>5KMPH</p>
+                <p>Wind speed</p>
+                </div>
+
           </div>
 
         </div>
